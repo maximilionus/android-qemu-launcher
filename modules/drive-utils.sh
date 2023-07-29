f434ea96a3d6345e55e884e1dd9f96a6cf9c9687
maximilionus <maximilionuss@gmail.com>
2023-07-30 02:51:32 +0300

    Enhance echo output of the `drive-utils` fncs

--- modules/drive-utils.sh
+++ modules/drive-utils.sh
@@ -1,27 +1,27 @@
 # Load kernel modules and mount the drive to a local folder inside the root
 # of current project
 driveutils_mount () {
-    echo "Loading the kernel modules"
+    echo "[ Loading the kernel modules ]"
     modprobe -v nbd max_part=8
 
-    echo "Mounting the disk image"
+    echo "[ Mounting the disk image ]"
     qemu-nbd --connect=/dev/nbd0 $DRIVE_PATH
     mkdir -v $DRIVE_MOUNT_PATH
     mount /dev/nbd0p1 $DRIVE_MOUNT_PATH
 
-    echo "Drive was successfully mounted to the '$(realpath $DRIVE_MOUNT_PATH)' directory"
+    echo "[ Drive was successfully mounted to the '$(realpath $DRIVE_MOUNT_PATH)' directory ]"
 }
 
 # Unmount the drive and unload the kernel modules
 driveutils_umount () {
-    echo "Unmounting the drive image"
+    echo "[ Unmounting the drive image ]"
     umount /dev/nbd0p1
     qemu-nbd --disconnect /dev/nbd0
 
-    echo "Unloading the kernel modules and removing the temp mount dir"
+    echo "[ Unloading the kernel modules and removing the temp mount dir ]"
     rmmod -v nbd
     rm -rfv $DRIVE_MOUNT_PATH
-    echo "Drive was successfully unmounted from the '$(realpath $DRIVE_MOUNT_PATH)' directory"
+    echo "[ Drive was successfully unmounted from the '$(realpath $DRIVE_MOUNT_PATH)' directory ]"
 }
 
 # Process the CLI args
